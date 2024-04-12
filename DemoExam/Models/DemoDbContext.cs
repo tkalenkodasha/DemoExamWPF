400128b793aeea70e49a1c9c39c550673e73da7a
tkalenkodasha <krasavica1912@mail.ru>
2024-04-12 16:44:24 +0700

    добавлены DemoDbContext(строки подключения) , пробное описание сущности Gender

---
+++ DemoExam/Models/DemoDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoExam.Models
+{
+    internal class DemoDbContext :DbContext
+    {
+        // Объявление статического поля для хранения экземпляра контекста базы данных
+        public static DemoDbContext _defaultContext;
+
+        // Метод для получения экземпляра контекста базы данных, создает новый экземпляр, если он еще не создан
+        public static DemoDbContext GetContext()
+        {
+            if (_defaultContext == null)
+            {
+                _defaultContext = new DemoDbContext();
+            }
+
+            return _defaultContext;
+        }
+
+        // Переопределение метода OnConfiguring для настройки параметров подключения к базе данных
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Строка подключения к базе данных MySQL
+            var connectionString = "host=localhost; user id=tklnk; password=123456; database=demodb;";
+            // Настройка контекста на использование MySQL с указанной версией сервера
+            optionsBuilder.UseMySql(connectionString, MySqlServerVersion.LatestSupportedServerVersion);
+        }
+
+        // Свойство, представляющее собой коллекцию сущностей типа Sotrudnik, для работы с данными сотрудников
+        public DbSet<Gender> Genders { get; set; }
+    }
+}
---
+++ DemoExam/Models/Gender.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoExam.Models
+{ 
+    // Класс Gender представляет собой модель для хранения информации о полах
+    public class Gender
+    {
+            // Уникальный идентификатор пола
+            public int Id { get; set; }
+            // Название пола
+            public string GenderName { get; set; }
+       
+
+    }
+}
