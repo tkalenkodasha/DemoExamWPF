de8cd72abf0d67e2074be6de38bd179404e167e4
tkalenkodasha <krasavica1912@mail.ru>
2024-04-14 20:30:59 +0700

    Добавлено главное окно, лого, ресурсы

--- DemoExam/Pages/Autho.xaml.cs
+++ DemoExam/Pages/Autho.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,19 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using DemoExam.Pages;
 
-namespace DemoExam
+namespace DemoExam.Pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для Autho.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Autho : Page
     {
-        public MainWindow()
+        public Autho()
         {
             InitializeComponent();
-            FrameMain.Navigate(new Autho());
         }
     }
 }
